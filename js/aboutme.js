ac38f8f10f5c7de333ddd00a1384413eaaa871ee
Andrew Dass <dassandrew3@gmail.com>
2026-02-24 14:51:38 -0500

    added github repositories from github using fetch

--- js/aboutme.js
+++ js/aboutme.js
@@ -4,7 +4,7 @@ let skillsSection = document.getElementById("skillsSection");
 
 for (let i = 0; i < skills.length; i++) {
     function createAndPostSkillDiv() {
-        let makeADiv = document.createElement("div" + String(i));
+        let makeADiv = document.createElement("div");
 
         makeADiv.innerHTML = skills[i];
 
